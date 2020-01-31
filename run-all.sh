2dcc9cf8f8f03de59400bba1e34efede7a6fa30d
John Santoro <john@johnsantoro.net>
2020-01-31 00:10:08 -0500

    updated run-all to include the new vim setup and switch users as needed for different sub-scripts

--- run-all.sh
+++ run-all.sh
@@ -3,16 +3,21 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/common.sh
 
+exit_unless_root
+
 printf "*** Running all scripts ***\n"
 
 printf "\n${BLUE_COLOR}1: install/install-software.sh${END_COLOR}\n"
 ($DIR/install/install-software.sh)
 
 printf "\n${BLUE_COLOR}2: configure/setup-ssh-rsa.sh${END_COLOR}\n"
-($DIR/configure/setup-ssh-rsa.sh)
+(runuser -l "$ORIGINAL_USER" -c "$DIR/configure/setup-ssh-rsa.sh")
 
 printf "\n${BLUE_COLOR}3: configure/check-ports.sh${END_COLOR}\n"
 ($DIR/configure/check-ports.sh)
+
+printf "\n${BLUE_COLOR}4: configure/setup-vim-pathogen.sh${END_COLOR}\n"
+(runuser -l "$ORIGINAL_USER" -c "$DIR/configure/setup-vim-pathogen.sh")
 
 printf "\n*** Finished running all scripts ***\n"
 
